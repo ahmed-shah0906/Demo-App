0040ea08b210ecf6318aafe9f0cb3a114a8c83d0
Shahrukh Ahmed <shahrukh.ahmed@nagarro.com>
2021-06-19 17:45:08 +0530

    Initial Commit

---
+++ DemoWebApp/DemoUT/DemoClassTest.cs
@@ -0,0 +1,16 @@
+﻿using DemoWebApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoUT
+{
+    [TestClass]
+    public class DemoClassTest
+    {
+        [TestMethod]
+        public void TestMethodWeb()
+        {
+            DemoClass demoClass = new DemoClass();
+            Assert.AreEqual(11, demoClass.Add(5, 6));
+        }
+    }
+}
---
+++ DemoWebApp/DemoWebApp/DemoClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebApp
+{
+    public class DemoClass
+    {
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
